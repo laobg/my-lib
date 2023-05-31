2f6a27ea51ededc7c73a78235dc95bbe4ad7e214
wangshiting <stwangcq@163.com>
2023-05-31 10:40:32 +0800

    feat: 地图

--- packages/components/src/echarts-map/echarts-map.vue
+++ packages/components/src/echarts-map/echarts-map.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="map-chart" ref="mapChart"></div>
+  <div class="map-wrapper">
+    <div class="map-chart" ref="mapChart"></div>
+    <div
+      class="map-chart popup-map"
+      :style="computedStyle"
+      ref="popoupMapChart"
+      @mouseleave="showPopupMap = false"
+    ></div>
+  </div>
 </template>
 <script lang="ts" setup>
-import { Ref, computed, markRaw, onMounted, ref } from 'vue';
-import chongqingJson from './chongqing.json';
+import { Ref, computed, markRaw, onMounted, reactive, ref } from 'vue';
+import chongqingJson from './chongqing_custom.json';
+import chongqingMainJson from './chongqing_main.json';
 import * as echarts from 'echarts';
 import './style';
 defineOptions({ name: 'EchartsMap' });
@@ -12,8 +21,23 @@ defineOptions({ name: 'EchartsMap' });
  * 地图实例
  */
 let mapChartInstance: echarts.ECharts | null = null;
+let popupMapChartInstance: echarts.ECharts | null = null;
 
+const showPopupMap = ref(false);
 const stationMap: Ref<any> = ref([]);
+
+const popupMapStyle = reactive({
+  top: '0px',
+  left: '0px',
+});
+const computedStyle = computed(() => {
+  return {
+    top: popupMapStyle.top,
+    left: popupMapStyle.left,
+    zIndex: showPopupMap.value ? 10 : -1,
+    opacity: showPopupMap.value ? 1 : 0,
+  };
+});
 // 地图
 const selectedStationIndex: Ref<number | null> = ref(null);
 const selectedStation = computed(() => {
@@ -22,8 +46,9 @@ const selectedStation = computed(() => {
     : '';
 });
 const mapChart = ref();
+const popoupMapChart = ref();
 // 展示地图
-function showMap(seriesData: any, lines: any[]) {
+function showMap(instance: any, mapName: string) {
   const options = {
     tooltip: {
       show: true, //不显示提示标签
@@ -34,13 +59,17 @@ function showMap(seriesData: any, lines: any[]) {
       },
     },
     geo: {
-      map: 'chongqing',
+      map: mapName,
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
       zlevel: 1,
       label: {
         show: true, //显示省份标签
         color: '#ffffff', //省份标签字体颜色
       },
-      layoutCenter: ['50%', '50%'],
+      // layoutCenter: ['50%', '50%'],
       layoutSize: '100%',
       aspectScale: 1, // 缩放
       itemStyle: {
@@ -112,11 +141,11 @@ function showMap(seriesData: any, lines: any[]) {
         // 经测试横向线 纬度 加减 0.04最合适 纵向线 则加减经度
         // 第一个为经度longitude 第二个为纬度latitude
         // 三根线时中间线 verticalAlign为middle 经纬度就是厂站的经纬度
-        data: lines,
+        // data: lines,
       },
     ],
   };
-  mapChartInstance!.setOption(options);
+  instance!.setOption(options);
 }
 onMounted(() => {
   mapChartInstance = markRaw(
@@ -125,11 +154,31 @@ onMounted(() => {
       renderer: 'svg',
     }),
   );
+  popupMapChartInstance = markRaw(
+    echarts.init(popoupMapChart.value, undefined, {
+      devicePixelRatio: 2,
+      renderer: 'svg',
+    }),
+  );
+  mapChartInstance.on('mouseover', function (params) {
+    if (params.name === '主城区') {
+      showPopupMap.value = true;
+      popupMapStyle.top = (params.event?.offsetY || 0) + 'px';
+      popupMapStyle.left = (params.event?.offsetX || 0) + 'px';
+    } else {
+      showPopupMap.value = false;
+    }
+  });
+  popupMapChartInstance.on('globalout', () => {
+    showPopupMap.value = false;
+  });
   // mapChartInstance!.off('click', handleFn);
   // mapChartInstance!.off('dblclick', doubleFn);
   // mapChartInstance!.on('click', 'series', handleFn);
   // mapChartInstance!.on('dblclick', 'series', doubleFn);
   echarts.registerMap('chongqing', chongqingJson as any);
-  showMap([], []);
+  echarts.registerMap('chongqingMain', chongqingMainJson as any);
+  showMap(mapChartInstance, 'chongqing');
+  showMap(popupMapChartInstance, 'chongqingMain');
 });
 </script>
